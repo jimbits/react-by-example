19551b598245ed70ffef21b3223454902a4126d1
jimbits <contactjamesfoley@gmail.com>
2020-01-02 11:30:45 -0700

    Animating In React With Greensock

--- react-animation/react-animation-gsap/src/App.js
+++ react-animation/react-animation-gsap/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from 'components/navigation/navbar';
- 
+import  './fonts/Montseratt.css'
  
  
 
--- react-animation/react-animation-gsap/src/components/navigation/navbar/Navbar.js
+++ react-animation/react-animation-gsap/src/components/navigation/navbar/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Bar = styled.nav `
- background-color:red;
- padding: 0.5rem 1rem;
- color:#ffffff;
-`
+const Bar = styled.nav`
+  background-color: #ff043f;
+  padding: 0.5rem 1rem;
+  color: #ffffff;
+`;
 const IconView = styled.ul `
    display:flex;
    justify-content:space-between;
@@ -17,9 +17,9 @@ export default function Navbar() {
     return (
       <Bar>
         <IconView>
-          <li>logo</li>
+          <li>Logo</li>
           <li>React Animation</li>
-          <li>menu</li>
+          <li>Menu Icon</li>
         </IconView>
         <ListView>
           <li>Route Something</li>
